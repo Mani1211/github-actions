f6e0fa25efd75cfa8885aa614f88b9f1c62c09b7
RohanRajendran-7 <rohanrajendran7@gmail.com>
2022-01-04 19:54:35 +0530

    Update:ChromeOptions

--- store.py
+++ store.py
@@ -25,11 +25,11 @@ SCREEN_HEIGHT=720
 
 # driver = webdriver.Chrome(executable_path= path) 
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--remote-debugging-port=9222')
-# chrome_options.add_argument('--no-sandbox') 
-# chrome_options.binary_location = "/usr/bin/google-chrome"
-# chrome_options.add_argument('--headless') 
-# chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--remote-debugging-port=9222')
+chrome_options.add_argument('--no-sandbox') 
+chrome_options.binary_location = "/usr/bin/google-chrome"
+chrome_options.add_argument('--headless') 
+chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome('./test_resources/chromedriver',chrome_options=chrome_options) 
 # driver.maximize_window()
 driver.set_window_size(SCREEN_WIDTH,SCREEN_HEIGHT)
