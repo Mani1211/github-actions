05ce1fe099e1a4cdc0e19e4db051eccf5e5e720c
RohanRajendran-7 <rohanrajendran7@gmail.com>
2022-01-04 13:48:09 +0530

    Update: Store.py

--- store.py
+++ store.py
@@ -27,7 +27,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox') 
 chrome_options.add_argument('--headless') 
 chrome_options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(executable_path= path) 
+driver = webdriver.Chrome(executable_path= path,options=chrome_options) 
 driver.maximize_window()
 driver.get("https://shopster.ai/en/login/")
 
